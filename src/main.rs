bca58f699a3114c0b2b1f0af486bdb16d003d929
DavidOConnor <david.alan.oconnor@gmail.com>
2018-12-16 12:30:34 -0500

    Added CORs support

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+
 extern crate serde;
 #[macro_use] extern crate serde_derive;
 extern crate serde_json;
+
+use rocket::http::Method;
+extern crate rocket_cors;
+use rocket_cors::{AllowedOrigins, AllowedHeaders};
 
 #[derive(Serialize)]
 struct Data {
@@ -28,5 +33,22 @@ fn data_api() -> String {
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index, data_api]).launch();
+    let (allowed_origins, failed_origins) = AllowedOrigins::some(
+        &["https://seed-example.herokuapp.com", "https://seed-rs.org"]
+        );
+    assert!(failed_origins.is_empty());
+
+    let options = rocket_cors::Cors {
+        allowed_origins: allowed_origins,
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        allow_credentials: true,
+        ..Default::default()
+    };
+
+
+    rocket::ignite()
+        .mount("/", routes![index, data_api])
+        .attach(options)
+        .launch();
 }
